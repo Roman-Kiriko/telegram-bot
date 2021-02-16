3bf7870954c0fd7e3967b871ec4327e642d95613
Roman Alma <okna.alma.seo3@gmail.com>
2021-02-16 15:58:42 +0300

    remove morgan

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const mongoose = require('mongoose') 
-const morgan = require('morgan')
 const userRouters = require('./routes/user')
 
 
